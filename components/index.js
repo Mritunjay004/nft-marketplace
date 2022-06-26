3c495944bafc6b3ec77cfc09c5c20ff694bfb01b
Mritunjay004 <mritunjaygupta004@gmail.com>
2022-06-27 00:38:28 +0530

    adding creators section

--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Button from './Button';
 import Banner from './Banner';
+import CreaterCard from './CreaterCard';
 
-export { Navbar, Footer, Button, Banner };
+export { Navbar, Footer, Button, Banner, CreaterCard };
