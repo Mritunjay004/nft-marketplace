78d9587389cf44b6efff7072395f95129bd85dd5
Mritunjay004 <mritunjaygupta004@gmail.com>
2022-06-27 17:54:32 +0530

    adding create-nft-page

--- components/index.js
+++ components/index.js
@@ -4,5 +4,6 @@ import Button from './Button';
 import Banner from './Banner';
 import CreaterCard from './CreaterCard';
 import NFTCard from './NFTCard';
+import Input from './Input';
 
-export { Navbar, Footer, Button, Banner, CreaterCard, NFTCard };
+export { Navbar, Footer, Button, Banner, CreaterCard, NFTCard, Input };
